de8c5a8ca9bc348efd6e5038cdcdd22fd21ccee5
EliRmz <elisaram@gmail.com>
2024-06-19 12:20:10 +0200

    Add delete project test

---
+++ spec/graphql/mutations/delete_project_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'DeleteProject Mutation', type: :request do
+  let!(:project) { Project.create(title: 'Test Project') } # Creates the project
+  let(:mutation) do
+    <<~GQL
+      mutation {
+        deleteProject(input: { id: #{project.id} }) {
+          success
+          errors
+        }
+      }
+    GQL
+  end
+
+  it 'deletes a project' do
+    expect {
+      post graphql_path, params: { query: mutation }
+      puts response.inspect
+      #binding.irb
+    }.to change { Project.count }.by(-1)
+
+    json_response = JSON.parse(response.body)
+    expect(json_response['data']['deleteProject']['success']).to be true
+    expect(json_response['data']['deleteProject']['errors']).to be_empty
+  end
+end
